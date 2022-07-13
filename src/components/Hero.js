3c267e31504d643e0f14fb37fdacae2b85834842
Chaman Bravo <chamanpro9@gmail.com>
2022-07-13 23:05:01 +0545

    feat: res from sanity

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import sanityClient from '../client'
 import Button from './Button'
-import heroImg from '../assets/hero-illustration.png'
 import blob from '../assets/blob.png'
-// import blobRT from '../assets/blobRT.png'
 import './Hero.css'
 
 function Hero() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "hero"]{
+      title,
+      description,
+      image{
+        asset->{
+          url
+        },
+      },
+    }`
+      )
+      .then((data) => setData(data))
+      .catch(console.error)
+  }, [])
+
   return (
     <>
       <div className='hero-container' id='hero'>
         <div className='hero-content'>
           <div className='hero-text'>
-            <h1 className='title-text'>Create your portfolio in minutes.</h1>
-            <p className='desc-text'>
-              With fiber, you can setup your own personal portfolio in minutes
-              with dozens of premade, beautiful templates.
-            </p>
+            <h1 className='title-text'>{data[0]?.title}</h1>
+            <p className='desc-text'>{data[0]?.description}</p>
             <div className='button'>
               <Button
                 className='btn-primary btn1'
@@ -25,16 +40,15 @@ function Hero() {
             </div>
           </div>
           <div className='hero-img'>
-            <img src={heroImg} alt='hero' className='hero-illustration' />
+            <img
+              src={data[0]?.image?.asset?.url}
+              alt='hero'
+              className='hero-illustration'
+            />
           </div>
         </div>
 
         <img src={blob} alt='gradient-blob' className='gradient-blob__img' />
-        {/* <img
-          src={blobRT}
-          alt='gradient-blob'
-          className='gradient-blob__imgRT'
-        /> */}
       </div>
     </>
   )
